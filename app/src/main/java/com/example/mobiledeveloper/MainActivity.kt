d1f74a3eec46b736f30e6bb5b16b0e08a9631f1e
dazzdizzduzz <dhaisalfarysy@gmail.com>
2025-03-12 23:44:33 +0700

    Ini Semuanya

---
+++ app/src/main/java/com/example/mobiledeveloper/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.mobiledeveloper
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.ComponentActivity
+import com.example.mobiledeveloper.databinding.ActivityMainBinding
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
